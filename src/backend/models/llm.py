bcd53eb345b88535f044ba430de6ed5b2adaec84
tlespargot <t_lespargot@apple.com>
2025-01-31 10:37:21 -0800

    Add rag and update llm

--- src/backend/models/llm.py
+++ src/backend/models/llm.py
@@ -1,5 +1,7 @@
 # import torch
+from dataclasses import dataclass
 from enum import Enum
+from typing import Any, Dict, List, Optional, TypedDict
 
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
@@ -13,20 +15,28 @@ class LLMType(str, Enum):
     mistral_7b = "mistralai/Mistral-7B-Instruct-v0.3"
 
 
+@dataclass
+class LLMResult:
+    last_answer: str
+    messages: List[Dict[str, str]]
+
+
 class LLMWrapper:
-    def __init__(self, llm_type: LLMType = LLMType.mistral_7b) -> None:
+    def __init__(self, llm_type: LLMType = LLMType.mistral_7b, device: str = "mps") -> None:
         self.model = AutoModelForCausalLM.from_pretrained(
-            llm_type.value, torch_dtype=torch.bfloat16, device_map="auto"
+            llm_type.value, torch_dtype=torch.bfloat16, device_map=device
         )
         self.tokenizer = AutoTokenizer.from_pretrained(llm_type.value)
         self.start_new_conversation()
+        self.device = device
+        # self.model.to(self.device)
 
-    def start_new_conversation(self):
+    def start_new_conversation(self) -> None:
         self.past_key_values = DynamicCache()
         self.max_cache_length = self.past_key_values.get_max_cache_shape()
-        self.messages = []
+        self.messages: List[Dict[str, str]] = []
 
-    def generate_answer(self, conversation, tools):
+    def generate_answer(self, prompt: str, tools: Optional[List[Any]] = None) -> LLMResult:
         # format and tokenize the tool use prompt
         self.messages.append({"role": "user", "content": prompt})
         inputs = self.tokenizer.apply_chat_template(
@@ -35,54 +45,55 @@ class LLMWrapper:
             return_tensors="pt",
             return_dict=True,
             tools=tools,
-        ).to(self.model.device)
+        ).to(self.device)
         if isinstance(self.past_key_values, SinkCache):
             inputs = {k: v[:, -self.max_cache_length :] for k, v in inputs.items()}
 
         input_length = inputs["input_ids"].shape[1]
 
         outputs = self.model.generate(
-            **inputs, do_sample=False, max_new_tokens=256, past_key_values=self.past_key_values
+            **inputs, max_new_tokens=256, past_key_values=self.past_key_values
         )
         completion = self.tokenizer.decode(outputs[0, input_length:], skip_special_tokens=True)
         self.messages.append({"role": "assistant", "content": completion})
-        print(completion)
-        return self.messages
-
-
-def get_current_weather(location: str, format: str):
-    """
-    Get the current weather
-
-    Args:
-        location: The city and state, e.g. San Francisco, CA
-        format: The temperature unit to use. Infer this from the users location. (choices: ["celsius", "fahrenheit"])
-    """
-    pass
+        # print(completion)
+        return LLMResult(last_answer=completion, messages=self.messages)
 
 
 if __name__ == "__main__":
-    tools = [get_current_weather]
+
+    def get_historical_information(question: str) -> None:
+        """
+        Get historical information about a given question.
+
+        Args:
+            question: The question to get the answer to.
+        """
+        pass
+
+    tools = [get_historical_information]
+    # tools = [get_current_weather, get_current_time]
+    # tools = [get_current_temperature, get_current_wind_speed]
     llm_wrapper = LLMWrapper()
     # format and tokenize the tool use prompt
 
     user_prompts = [
-        ("Hello, I'm Thomas, what's your name?", None),
+        ("Hello, I'm Thomas, what's your name?", tools),
         ("Btw, yesterday I was on a rock concert.", None),
         ("What's my name?", None),
         ("What's the weather like in Paris?", None),
     ]
     user_prompts = [
-        ("Hello, I'm Thomas, what's your name?", None),
-        ("Est-ce que tu sais parler francais?", None),
-        ("Quelles autres langues sais-tu parler?", None),
-        ("What's my name?", None),
+        ("Hello, I'm Thomas, what's your name?", tools),
+        # ("Est-ce que tu sais parler francais?", tools),
+        # ("Quelles autres langues sais-tu parler?", tools),
+        # ("What's my name?", None),
+        ("When is born Emannuel Macron?", tools),
     ]
-    messages = []
     for prompt, tools in user_prompts:
         messages = llm_wrapper.generate_answer(prompt, tools)
 
-    print(messages)
+    print(messages.last_answer)
     # meta-llama 3.2
     # gemma 2
     # phi 3.5
---
+++ src/backend/models/rag.py
@@ -0,0 +1,62 @@
+from typing import Iterable, Tuple
+
+from src.backend.models.encoder import BertPath
+from src.backend.models.llm import LLMType, LLMWrapper
+from src.backend.models.retriever import BiEncoderRetriever
+
+
+class RAGWrapper:
+    def __init__(self, encoder_type: BertPath, llm_type: LLMType) -> None:
+        self.retriver = BiEncoderRetriever(encoder_type)
+        self.llm = LLMWrapper(llm_type)
+        self._initialize_llm()
+
+    def prepare_corpus(self, corpus: Iterable[Tuple[str, str]]) -> None:
+        self.corpus = {name: doc for name, doc in corpus}
+        self.retriver.embed_corpus(corpus)
+
+    def _initialize_llm(self) -> None:
+        initial_message = """You are a helpful chatbot. You will be provided each time a document and a question.
+            Use only the following pieces of context to answer the question. Don't make up any new information!"""
+        _ = self.llm.generate_answer(initial_message, None)
+
+    def format_prompt(self, retrieved_chunk: str, question: str) -> str:
+        prompt = f"""
+        Context information is below.
+        ---------------------
+        {retrieved_chunk}
+        ---------------------
+        Given the context information and not prior knowledge, answer the query.
+        Query: {question}
+        Answer:
+        """
+        return prompt
+
+    def answer_question(self, question: str) -> str:
+        best_document_name = self.retriver.retrieve(question)[0][0]
+        print(best_document_name)
+        best_document = self.corpus[best_document_name]
+
+        prompt = self.format_prompt(best_document, question)
+
+        llm_output = self.llm.generate_answer(prompt=prompt)
+        return llm_output.last_answer
+
+
+if __name__ == "__main__":
+    corpus = [
+        (
+            "Emmanuel Macron",
+            "Emmanuel Macron was born on December 21, 1977, in Amiens, France. He is a French politician who served as the President of France from 2017 to 2022. Before his presidency, he worked as an investment banker and was an inspector of finances for the French government.",
+        ),
+        ("TSNE", "TSNE is a dimensionality reduction algorithm created by Laurens van Der Maaten"),
+        ("horse", "The horse is white."),
+    ]
+    rag_wrapper = RAGWrapper(BertPath.modern_bert, llm_type=LLMType.mistral_7b)
+
+    rag_wrapper.prepare_corpus(corpus)
+
+    queries = ["When is born Emannuel Macron?", "What color is the horse?", "What is TSNE?"]
+
+    for query in queries:
+        print(rag_wrapper.answer_question(query))
